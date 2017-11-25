47c00a190f0f7c2012e403d02739dc2bf5b2ba9a
kharazzoro <kha.1991.14.2017@gmail.com>
2017-11-25 02:02:03 +0000

    remove unwanted codes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-const articleDB= require('../dbClient/article')
-const categoryDB= require('../dbClient/category')
-
 /* GET home page. */
 router.get('/', (req, res, next)=> {
   let callbac=()=>{
     res.return('/')
   }
-  //articleDB({'title':'ali','fullContent':'ali is cool man'},callbac)
-  categoryDB({'title':'celtic','shortDescription':'sportive'},callbac);
-  res.render('index');
+    res.render('index');
 });
 
 module.exports = router;
