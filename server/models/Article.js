02bce8c3781c9cda72cecf4a55438de5eff078df
kharazzoro <kha.1991.14.2017@gmail.com>
2017-11-26 21:05:29 +0000

    add more field in article form

--- server/models/Article.js
+++ server/models/Article.js
@@ -4,6 +4,14 @@ const {Schema}= mongoose;
 const schema=new Schema({
     title:String,
     fullContent:String,
+    image:{
+        type:String,
+        default:'http://pm1.narvii.com/5856/dac779189dc23ed6aad98d850ace0b397cc14c5a_hq.jpg'
+    },
+    video:String,
+    date:String,
+    publisherName:String,
+    articleType:String,
     visible:{
         type:Boolean,
         default:true
