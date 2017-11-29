ac540b79e41418ec63b42f09aac8664d8befab9a
kharazzoro <kha.1991.14.2017@gmail.com>
2017-11-29 10:47:28 +0000

    create react components

--- client/src/container/app/App.js
+++ client/src/container/app/App.js
@@ -3,6 +3,7 @@ import logo from '../../logo.svg';
 import './App.css';
 import Menu from '../../components/menu/menu'
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Categories from '../../components/category/categoryCard'
 
 class App extends Component {
   render() {
@@ -12,7 +13,7 @@ class App extends Component {
       <div>
         <Menu />
         
-        <Route exact path="/" component={Menu} />
+        <Route exact path="/" component={Categories} />
         <Route path="/about" component={Menu} />
         <Route path="/categories" exact component={Menu} /> 
         <Route path="/articles/:articleId" component={Menu} />
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './container/app/App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './container/app/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
