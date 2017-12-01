0d588fe07a2f87ac88352c61a5073140da1f41b2
kharazzoro <kha.1991.14.2017@gmail.com>
2017-12-01 01:28:41 +0000

    article list

--- client/src/components/article/articleCard.js
+++ client/src/components/article/articleCard.js
@@ -7,16 +7,25 @@ import './articleCard.css'
 const ArticleCard = (props) => {
     return (
 
-        <Card id="articleCard">
-            <CardImg top width="30%" src={props.img} alt="Card image cap" />
-            <CardTitle>{props.title}</CardTitle>
-            <CardSubtitle>{props.publisherName}</CardSubtitle>
-            <CardSubtitle>{props.articleType}</CardSubtitle>
-            <a href={props.id}><Button>View Article</Button></a>
+    
+         <div  class="articleBox ">
+                <div class="articleBox-left ">
+                <h2>Writer:</h2>
+                <img src={props.img} alt="Card image cap" />
+                <h3>{props.publisherName}</h3>
+                <h3>{props.date}</h3>
+                </div>
 
-        </Card>
-
-    );
+                <div class="articleBox-right ">
+                <h2>Article:</h2>
+                <img src={props.img} alt="Card image cap" />
+                <h1>{props.title}</h1>
+                <h3>{props.publisherName}</h3>
+                <h4>{props.articleType}</h4>
+                <a href={props.id}><Button>View Article</Button></a>
+                </div>
+         </div>  
+     );
 };
 
 export default ArticleCard;
